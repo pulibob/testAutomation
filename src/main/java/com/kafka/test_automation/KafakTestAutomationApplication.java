5ad4dfc8993bd5e2d272eed95459420946b048cf
Mustaq <developeratvalley@gmail.com>
2025-02-25 22:31:42 +0530

    Initial commit spring boot app

---
+++ src/main/java/com/kafka/test_automation/KafakTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.test_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafakTestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafakTestAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/test_automation/KafakTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.test_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafakTestAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
