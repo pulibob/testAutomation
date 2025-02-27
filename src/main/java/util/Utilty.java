bf033c68ab3d796aa0b2ff83d880bd119f4d9fd9
Mustaq <developeratvalley@gmail.com>
2025-02-27 21:03:07 +0530

    Json reader from csv

---
+++ src/main/java/util/Utilty.java
@@ -0,0 +1,84 @@
+package util;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Component
+public class Utilty {
+
+    public static void main(String[] args) {
+        System.out.println(getJsonString("Testcase 1"));
+    }
+
+
+    public static String getJsonString(String testCaseName){
+       return updateIdFromJsonString(getJsonStringFromCSV(testCaseName));
+    }
+
+
+    private static String getJsonStringFromCSV(String testCaseName) {
+        try {
+            ClassPathResource resource = new ClassPathResource("csv/test-data.xlsx");
+            InputStream file = resource.getInputStream();
+
+            Workbook workbook = new XSSFWorkbook(file);
+            Sheet sheet = workbook.getSheetAt(0); // Read first sheet
+
+            for (Row row : sheet) {
+                Cell cellA = row.getCell(0); // Column A (Index 0)
+                Cell cellB = row.getCell(1); // Column B (Index 1)
+
+                if (cellA != null && cellB != null) {
+                    String columnAValue = cellA.getStringCellValue().trim();
+                    if (columnAValue.equals(testCaseName)) {
+                        workbook.close();
+                        file.close();
+                        return cellB.getStringCellValue(); // Return matching Column B value
+                    }
+                }
+            }
+            workbook.close();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "No Match Found"; // Return if no match is found
+    }
+
+    private static String updateIdFromJsonString(String oldJson) {
+
+        System.out.println("Before Update:::::" + oldJson);
+        String updatedJson="";
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(oldJson);
+
+            String newId = UUID.randomUUID().toString();
+
+            ((ObjectNode) rootNode).put("id", newId);
+
+            updatedJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode);
+
+            System.out.println("After Update JSON: \n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return updatedJson;
+    }
+
+}
---
+++ src/test/java/jUnitTests/TestCryptoBuyAndSell.java
@@ -0,0 +1,11 @@
+package jUnitTests;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestCryptoBuyAndSell {
+
+//    @Autowired
+//    public Utilty Utilty;
+
+
+}
